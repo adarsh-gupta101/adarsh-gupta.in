4061aa05cf1905a402f4d2403bbe1524199cf41d
adarsh-gupta101 <adarshgupta2468@gmail.com>
2024-09-22 17:52:06 +0530

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,17 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { Poppins } from "next/font/google"
 
 import { Analytics } from "@vercel/analytics/react"
+import Head from "next/head";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
 
 export const metadata: Metadata = {
   title: "Adarsh Gupta",
   description: "A Typical Indian Software Developer who loves to code and write blogs.",
+  icons:["/favicon.ico"]
+  // add og
+  
+
 };
 
 export default function RootLayout({
@@ -21,6 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Analytics />
+      {/* set logo */}
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={poppins.className}>
       <ThemeProvider
             attribute="class"
