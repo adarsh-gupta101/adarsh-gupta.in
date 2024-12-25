5330b7faf110257c8ccd77baa3c62fd38053ddbf
adarsh-gupta101 <adarshgupta2468@gmail.com>
2024-12-25 23:43:21 +0530

    ui + exp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
       <body className={poppins.className}>
       <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
+            defaultTheme="dark"
+            // enableSystem
             disableTransitionOnChange
           >
         {children}
